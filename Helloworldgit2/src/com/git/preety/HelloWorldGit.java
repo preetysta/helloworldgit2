d355b61017ac69da4fc15e6bef4ae11af6d7a848
preetysta <preetysta@gmail.com>
2017-12-26 16:04:19 -0500

    Initial check in.

---
+++ Helloworldgit2/src/com/git/preety/HelloWorldGit.java
@@ -0,0 +1,10 @@
+package com.git.preety;
+
+public class HelloWorldGit {
+
+	public void display() {
+		System.out.println("We create the project first in eclipse and then github");
+		
+	}
+
+}
---
+++ Helloworldgit2/src/com/git/preety/Main.java
@@ -0,0 +1,17 @@
+package com.git.preety;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HelloWorldGit hw = new HelloWorldGit();
+		displayGreeting();
+		hw.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Welcome to GitHub!");
+		
+	}
+
+}
